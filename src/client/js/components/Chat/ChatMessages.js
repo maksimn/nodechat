5ca5656d5d790cd3f6ee3135e05b44e72c510290
maksimn <maksim1989sovunion@yandex.ru>
2017-06-24 06:26:12 +0300

    Поведение сообщений чата при получении нового и их прокрутке

--- src/client/js/components/Chat/ChatMessages.js
+++ src/client/js/components/Chat/ChatMessages.js
@@ -6,18 +6,46 @@ import ChatMessage from './ChatMessage';
 import {getChatMessages} from '../../actions/chat';
 
 class ChatMessages extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.chatMessagesDiv = null;
+        this.isScrolledFromBottomPos = false;
+    }
+
     componentWillMount() {
         this.props.dispatch(getChatMessages());
     }
 
+    componentDidMount() {
+        setInterval(this.updateScroll.bind(this), 300);
+    }
+
+    updateScroll() {
+        const {chatMessagesDiv} = this;
+
+        if (!(this.isScrolledFromBottomPos)) {
+            chatMessagesDiv.scrollTop = chatMessagesDiv.scrollHeight;
+        }
+    }
+
+    scrollHandler(e) {
+        const chatMessagesDiv = e.target;
+        this.isScrolledFromBottomPos = 
+            chatMessagesDiv.clientHeight + chatMessagesDiv.scrollTop !== 
+            chatMessagesDiv.scrollHeight;
+    }
+
     render() {
         const messages = this.props.messages ? this.props.messages : [];
+        const chatMessages = messages.map((message, ind) => (
+            <ChatMessage key={ind} message={message} />
+        ));
 
-        return <div className="chat-room__chat-messages">
-            {messages.map(
-                (message, ind) => (
-                    <ChatMessage key={ind} message={message} />                
-                ))}
+        return <div className="chat-room__chat-messages"
+                    onScroll={ this.scrollHandler.bind(this) }
+                    ref={ div => { this.chatMessagesDiv = div; }}>
+            {chatMessages}
         </div>;
     }
 }
