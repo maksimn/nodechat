07c89801dd3a014f244d668496717276a7aff478
maksimn <maksim1989sovunion@yandex.ru>
2017-07-05 15:28:27 +0300

    Небольшой рефакторинг кода сокетов

--- src/server/app.js
+++ src/server/app.js
@@ -3,12 +3,10 @@ require('./config/appConfig');
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const sockets = require('./sockets');
 
 const app = express();
 const port = process.env.PORT;
 const server = require('http').createServer(app);
-const io = require('socket.io')(server);
 
 const publicPath = path.join(__dirname, '../../output');
 
@@ -18,7 +16,8 @@ app.use(bodyParser.json());
 const appRouter = require('./routes');
 app.use(appRouter);
 
-sockets(io);
+const sockets = require('./sockets');
+sockets(server);
 
 server.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
--- src/server/sockets.js
+++ src/server/sockets.js
@@ -1,9 +1,12 @@
+const socketIO = require('socket.io');
 const mockRepository = require('./mockRepository');
 const repository = mockRepository;
 
 const NEW_CHAT_MESSAGE = 'NEW_CHAT_MESSAGE';
 
-module.exports = io => {
+module.exports = server => {
+    const io = socketIO(server);
+    
     io.on('connection', socket => {
         socket.on(NEW_CHAT_MESSAGE, message => {
 
