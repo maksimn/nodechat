2ca1e0f3b1c6c54449e722c716436d633789998e
maksimn <maksim1989sovunion@yandex.ru>
2017-07-30 23:29:03 +0300

    Сборка серверного node.js кода через webpack

--- src/server/app.js
+++ src/server/app.js
@@ -2,15 +2,12 @@ require('./config/appConfig');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
 
 const app = express();
 const port = process.env.PORT;
 const server = require('http').createServer(app);
 
-const publicPath = path.join(__dirname, '../../output');
-
-app.use(express.static(publicPath));
+app.use(express.static('./output'));
 app.use(bodyParser.json());
 
 const appRouter = require('./routes');
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,42 @@
 var path = require('path');
+var nodeExternals = require('webpack-node-externals');
 
-module.exports = {
-    context: path.join(__dirname, "src/client"),
+var LOADERS = [
+    {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+            presets: ['react', 'es2015', 'stage-0'],
+            plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties'],
+        }
+    },
+    {
+        test: /\.json$/,
+        loader: 'json-loader'
+    }
+];
+
+module.exports = [{
     devtool: "inline-source-map",
-    entry: "./index.js",
+    entry: "./src/client/index.js",
     module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['react', 'es2015', 'stage-0'],
-                    plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties'],
-                }
-            }
-        ]
+        loaders: LOADERS
     },
     output: {
         path: path.join(__dirname, "output"),
         filename: "client.js"
     }
-};
+}, {
+    target: "node",
+    externals: [nodeExternals()],
+    devtool: "inline-source-map",
+    entry: "./src/server/app.js",
+    module: {
+        loaders: LOADERS
+    },
+    output: {
+        path: path.join(__dirname, "output"),
+        filename: "server.js"
+    }
+}];
